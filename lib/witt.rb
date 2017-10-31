a9c4192ab02d01f778639202c2ec056079ba04fa
Pedro Vereza <pdrvereza@gmail.com>
2017-10-31 10:40:10 -0200

    Hooks things up

--- lib/witt.rb
+++ lib/witt.rb
@@ -1,4 +1,23 @@
 require "tzinfo"
 
 require "witt/version"
+require "witt/iata"
 require "witt/world_clock"
+require "witt/output"
+
+module Witt
+  extend self
+
+  def time_in(airports)
+    world_clock = WorldClock.new
+    output = Output.new
+
+    airports.each do |airport|
+      airport = airport.upcase
+      timezone = @IATA[airport]
+      time = world_clock.time_in(timezone)
+
+      output.print(time, airport, timezone)
+    end
+  end
+end
